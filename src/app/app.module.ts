b991c1beee3c38ce34992d2feb296e681704f652
derio.costa <deriocosta@searchtecnologia.com.br>
2023-01-12 12:09:52 -0300

    Criação de novo card de herois

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
 @NgModule({
   declarations: [
     AppComponent,
