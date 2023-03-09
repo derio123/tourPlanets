15b4dfead8d0968191e7b482151e9df90ba26903
derio.costa <deriocosta@searchtecnologia.com.br>
2023-03-09 17:15:34 -0300

    Novas imagens

--- src/data/mock-heroes.ts
+++ src/data/mock-heroes.ts
@@ -1,19 +1,24 @@
 import { Hero } from '../data/hero';
 
 export const HEROES: Hero[] = [
-	{id: 1, name: 'Yara', img: '../../assets/img/WWYara.png' },
-	{id: 2, name: 'Diana', img: '../../assets/img/WWDiana.png' },
-	{id: 3, name: 'Elektra', img: '../../assets/img/elektra2.png' },
-	{id:4, name: 'Iron Man', img: '../../assets/img/ironman.jpg'},
-	{id: 5, name: 'Captain America', img: '../../assets/img/captainAmerica.jpeg'},
-	{id: 6, name: 'Captain Marvel', img: '../../assets/img/captainMarvel.jpeg'},
-	{id: 7, name: 'Iron Heart', img: '../../assets/img/ironHeart2.jpg'},
-	{id: 8, name: 'Thor', img: '../../assets/img/thor.jpg'},
-	{id: 9, name: 'Black Window', img: '../../assets/img/blackwindow.jpg'},
-	{id: 10, name: 'Spider Marvel', img: '../../assets/img/spiderman.jpeg'},
-	{id: 11, name: 'Doctor Strange', img: '../../assets/img/doctorstrange1.png'},
-	{id: 12, name: 'Scalet Witch', img: '../../assets/img/scaletwitch1.png' },
-	{id: 13, name: 'Hulk', img: '../../assets/img/hulk.png' },
-	{id: 14, name: 'o', img: 'x' },
-	{id: 15, name: 'o', img: 'x' },
+	{ id: 1, name: 'Yara', img: '../../assets/img/WWYara3.png' },
+	{ id: 2, name: 'AERO (LEI LING)', img: '../../assets/img/Aero.png' },
+	{ id: 3, name: 'Diana', img: '../../assets/img/WWDiana3.png' },
+	{ id: 4, name: 'Elektra', img: '../../assets/img/elektra3.png' },
+	{ id: 5, name: 'Iron Man', img: '../../assets/img/ironman.png' },
+	{ id: 6, name: 'Captain America', img: '../../assets/img/captainAmerica.png' },
+	{ id: 7, name: 'Captain Marvel', img: '../../assets/img/CMCarol.png' },
+	{ id: 8, name: 'Iron Heart', img: '../../assets/img/ironHeart4.png' },
+	{ id: 9, name: 'Thor', img: '../../assets/img/thor.png' },
+	{ id: 10, name: 'Black Window', img: '../../assets/img/blackwindow.png' },
+	{ id: 11, name: 'Spider Marvel', img: '../../assets/img/spiderman.png' },
+	{ id: 12, name: 'Doctor Strange', img: '../../assets/img/doctorstrange1.png' },
+	{ id: 13, name: 'Scalet Witch', img: '../../assets/img/scaletwitch1.png' },
+	{ id: 14, name: 'Hulk', img: '../../assets/img/hulk.png' },
+	{ id: 15, name: 'Arclight', img: '../../assets/img/Arclight.png' },
+	{ id: 16, name: 'ASP', img: '../../assets/img/Asp.png' },
+	/* {id: 15, name: 'o', img: 'x' }, */
+	/* {id: 15, name: 'o', img: 'x' }, */
+	/* {id: 15, name: 'o', img: 'x' }, */
+	/* {id: 15, name: 'o', img: 'x' }, */
 ];
