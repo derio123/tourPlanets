5bcb1ae230b25c4f1d48fc0c75dafccbd1a46fdf
derio123 <deriomatheus6@gmail.com>
2024-04-29 11:20:37 -0300

    Mundanças no deletar

--- src/data/mock-heroes.ts
+++ src/data/mock-heroes.ts
@@ -1,25 +1,52 @@
 import { Hero } from '../data/hero';
 
 export const HEROES: Hero[] = [
-	{ id: 1, name: 'Yara', img: '../../assets/img/WWYara2.png' },
-	{ id: 2, name: 'AERO (LEI LING)', img: '../../assets/img/Aero.png' },
-	{ id: 3, name: 'Diana', img: '../../assets/img/WWDiana3.png' },
-	{ id: 4, name: 'Arclight', img: '../../assets/img/Arclight.png' },
-	{ id: 5, name: 'ASP', img: '../../assets/img/Asp.png' },
-	{ id: 6, name: 'Elektra', img: '../../assets/img/elektra3.png' },
-	{ id: 7, name: 'Iron Man', img: '../../assets/img/ironman.png' },
-	{ id: 8, name: 'Captain America', img: '../../assets/img/captainAmerica.png' },
-	{ id: 9, name: 'Captain Marvel', img: '../../assets/img/CMCarol4.png' },
-	{ id: 10, name: 'Iron Heart', img: '../../assets/img/ironHeart4.png' },
-	{ id: 11, name: 'Thor', img: '../../assets/img/thor.png' },
-	{ id: 12, name: 'Black Window', img: '../../assets/img/blackwindow.png' },
-	{ id: 13, name: 'Spider Marvel', img: '../../assets/img/spiderman.png' },
-	{ id: 14, name: 'Doctor Strange', img: '../../assets/img/doctorstrange1.png' },
-	{ id: 15, name: 'Scalet Witch', img: '../../assets/img/scaletwitch1.png' },
-	{ id: 16, name: 'Hulk', img: '../../assets/img/hulk.png' },
-
-	/* {id: 15, name: 'o', img: 'x' }, */
-	/* {id: 15, name: 'o', img: 'x' }, */
-	/* {id: 15, name: 'o', img: 'x' }, */
-	/* {id: 15, name: 'o', img: 'x' }, */
+	{
+		id: 1, name: 'Yara', img: '../../assets/img/WWYara2.png', power: ''
+	},
+	{
+		id: 2, name: 'AERO (LEI LING)', img: '../../assets/img/Aero.png', power: ''
+	},
+	{
+		id: 3, name: 'Diana', img: '../../assets/img/WWDiana3.png', power: ''
+	},
+	{
+		id: 4, name: 'Arclight', img: '../../assets/img/Arclight.png', power: ''
+	},
+	{
+		id: 5, name: 'ASP', img: '../../assets/img/Asp.png', power: ''
+	},
+	{
+		id: 6, name: 'Elektra', img: '../../assets/img/elektra.png', power: ''
+	},
+	{
+		id: 7, name: 'Iron Man', img: '../../assets/img/ironman.png', power: ''
+	},
+	{
+		id: 8, name: 'Captain America', img: '../../assets/img/captainAmerica.png', power: ''
+	},
+	{
+		id: 9, name: 'Captain Marvel', img: '../../assets/img/CMCarol4.png', power: ''
+	},
+	{
+		id: 10, name: 'Iron Heart', img: '../../assets/img/ironHeart4.png', power: ''
+	},
+	{
+		id: 11, name: 'Thor', img: '../../assets/img/thor.png', power: ''
+	},
+	{
+		id: 12, name: 'Black Window', img: '../../assets/img/blackwindow.png', power: ''
+	},
+	{
+		id: 13, name: 'Spider Marvel', img: '../../assets/img/spiderman.png', power: ''
+	},
+	{
+		id: 14, name: 'Doctor Strange', img: '../../assets/img/doctorstrange1.png', power: ''
+	},
+	{
+		id: 15, name: 'Scalet Witch', img: '../../assets/img/scaletwitch1.png', power: ''
+	},
+	{
+		id: 16, name: 'Hulk', img: '../../assets/img/hulk.png', power: ''
+	},
 ];
