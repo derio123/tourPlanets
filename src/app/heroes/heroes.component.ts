c2bc47224e32836430b8ba1973b06493f9310c8f
derio123 <deriomatheus6@gmail.com>
2025-11-05 12:44:16 -0300

    Ajuste no ambiente dev e para outros  projetos

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,11 +2,13 @@ import { MessageService } from './../service/messages/message.service';
 import { HeroService } from './../service/hero.service';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../data/hero';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
 
